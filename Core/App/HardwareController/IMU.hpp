32094aaa383130f14c719e93035033dc5d842ac4
onlydcx <rin.hanaoka@icloud.com>
2023-12-23 15:19:57 +0900

    disable IMU & MPU6500 files

--- Core/App/HardwareController/IMU.hpp
+++ Core/App/HardwareController/IMU.hpp
@@ -1,43 +1,43 @@
-/*
- *  IMU.hpp
- *
- *  Created on: Jun 21, 2023
- *      Author: SHIMOTORI Haruki
- *  https://github.com/shimotoriharuki/reRo_robotrace_board_public/blob/master/main_code/Core/Inc/IMU.hpp
- */
+// /*
+//  *  IMU.hpp
+//  *
+//  *  Created on: Jun 21, 2023
+//  *      Author: SHIMOTORI Haruki
+//  *  https://github.com/shimotoriharuki/reRo_robotrace_board_public/blob/master/main_code/Core/Inc/IMU.hpp
+//  */
 
-#ifndef INC_IMU_HPP_
-#define INC_IMU_HPP_
+// #ifndef INC_IMU_HPP_
+// #define INC_IMU_HPP_
 
-#include "stm32f4xx_hal.h"
+// #include "stm32f4xx_hal.h"
 
-#define R_IMU 0.1 //0.03 Lowpath filter constant. The smaller it is, the more effective/
+// #define R_IMU 0.1 //0.03 Lowpath filter constant. The smaller it is, the more effective/
 
-class IMU{
-private:
-	int16_t xa_, ya_, za_, xg_, yg_, zg_;
-	float omega_;
-	float offset_;
-	float theta_;
-	float theta_10mm_;
+// class IMU{
+// private:
+// 	int16_t xa_, ya_, za_, xg_, yg_, zg_;
+// 	float omega_;
+// 	float offset_;
+// 	float theta_;
+// 	float theta_10mm_;
 
-public:
-	IMU();
-	uint8_t init();
-	void updateValues();
-	float getOmega();
-	void calibration();
+// public:
+// 	IMU();
+// 	uint8_t init();
+// 	void updateValues();
+// 	float getOmega();
+// 	void calibration();
 
-	float getOffsetVal();
+// 	float getOffsetVal();
 
-	float getTheta();
+// 	float getTheta();
 
-	void correctionTheta(float);
-	void clearTheta();
+// 	void correctionTheta(float);
+// 	void clearTheta();
 
-	float getTheta10mm();
-	void clearTheta10mm();
+// 	float getTheta10mm();
+// 	void clearTheta10mm();
 
-};
+// };
 
-#endif /* INC_IMU_HPP_ */
+// #endif /* INC_IMU_HPP_ */
