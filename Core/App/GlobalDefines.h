24b140b544dae1048e9963376df5c805e2dd029d
onlydcx <rin.hanaoka@icloud.com>
2024-01-01 23:35:02 +0900

    fix

--- Core/App/GlobalDefines.h
+++ Core/App/GlobalDefines.h
@@ -1,8 +1,8 @@
 #ifndef __APP_GLOBAL_DEFINES_H__
 #include __APP_GLOBAL_DEFINES_H__
 
-#define MAIN_CONTROLL_CYCLE 0.001
-#define IMU_CONTROLL_CYCLE 0.000001
+#define MAIN_CONTROLL_CYCLE 0.001   // 1KHz
+#define IMU_CONTROLL_CYCLE 0.000001 // 100KHZ
 
 #define deg_to_rad(deg) (((deg) / 360) * 2 * M_PI)
 #define rad_to_deg(rad) (((rad) / 2 / M_PI) * 360)
