637d531dc7c5e19aa23e2c9c33e945587a9fe62f
onlydcx <rin.hanaoka@icloud.com>
2024-01-07 01:25:30 +0900

    [add] Battery defines

--- Core/App/GlobalDefines.h
+++ Core/App/GlobalDefines.h
@@ -9,6 +9,9 @@
 #define deg_to_rad(deg) (((deg) / 360) * 2 * M_PI)
 #define rad_to_deg(rad) (((rad) / 2 / M_PI) * 360)
 
+#define BATT_MIN_V 10.8
+#define BATT_MAX_V 12.6
+
 #ifndef DEBUG
 #define DEBUG
 #endif
