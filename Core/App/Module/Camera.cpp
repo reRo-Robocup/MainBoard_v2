388dac971877dc6f560450fe2a87aaa82f31b795
onlydcx <rin.hanaoka@icloud.com>
2024-01-09 19:45:29 +0900

    [moved] Camera

---
+++ Core/App/Module/Camera.cpp
@@ -0,0 +1,21 @@
+/*
+ *  Camera.cpp
+ *
+ *  Created on: Jan 1, 2024
+ * 
+ *  Author: User, G4T1PR0
+ */
+
+#include <Camera.hpp>
+
+camera::camera(MAL* mcu) {
+    _mcu = mcu;
+}
+
+void camera::init() {
+
+}
+
+void camera::updateFPS() {
+    
+}
---
+++ Core/App/Module/Camera.hpp
@@ -0,0 +1,31 @@
+/*
+ *  Camera.cpp
+ *
+ *  Created on: Jan 1, 2024
+ * 
+ *  Author: User, G4T1PR0
+ */
+
+#ifndef __CAMERA_CPP__
+#define __CAMERA_CPP__
+
+#include <baseMcuAbstractionLayer.hpp>
+
+class camera {
+    public:
+        camera(MAL* mcu);
+        void init();
+        void updateFPS();
+        bool AttackColor;
+
+        struct Object {
+            int16_t angle;
+            uint8_t distance;
+            bool enable;
+        };
+
+    private:
+        MAL* _mcu;
+};
+
+#endif
