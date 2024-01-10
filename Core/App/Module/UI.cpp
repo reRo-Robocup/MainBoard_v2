c61aecf908acdad80ffca0e0e093fcc0103832e0
onlydcx <rin.hanaoka@icloud.com>
2024-01-11 00:07:49 +0900

    [update] UI

---
+++ Core/App/Module/UI.cpp
@@ -0,0 +1,17 @@
+/*
+ * UI.cpp
+ *
+ *  Created on: Jan 10, 2024
+ */
+
+#include <UI.hpp>
+#include <baseMcuAbstractionLayer.hpp>
+
+Buzzer::Buzzer(MAL* mcu) {
+    _mcu = mcu;
+}
+
+void Buzzer::on(uint16_t pulse, uint8_t ms) {
+    pulse /= _mcu->getPWMConpare(MAL::Peripheral_PWM::Buzzer);
+    _mcu->pwmSetDuty(MAL::Peripheral_PWM::Buzzer, pulse);
+}
