ce106d1389db1b1485ba8535b23f1a21ba8c65f5
onlydcx <rin.hanaoka@icloud.com>
2024-01-13 02:02:55 +0900

    [update] アルゴリズム

--- Core/App/app_main.cpp
+++ Core/App/app_main.cpp
@@ -16,65 +16,71 @@
 #include <Module/UI.hpp>
 #include <Module/MotorControll.hpp>
 
-void app_init() {
+#include <McuAbstractionLayer/stm32f446AbstractionLayer.hpp>
 
+stm32f446AbstractionLayer* mcu;
+
+camera cam(mcu);
+LineSensor line(mcu);
+MPU6500 imu(mcu);
+MotorControll motor(mcu);
+
+void app_init() {
+    cam.init();
+    line.init();
+    imu.init();
+    motor.init();
 }
 
 void app_main() {
+
     app_init();
 
-    // const int isBallFront = 30;
-    // int16_t angle;
-    // uint16_t speed;
-
-    // uint16_t Ball_threshold = 3078;
+    uint16_t BallAngle = 0;
+    uint8_t isBallFront = 5;
+    uint16_t angle = 0;
 
     while (1) {
 
-        // // スピード決定 実装中
-        // speed = devices.mcu->adcGetValue(MAL::Peripheral_ADC::BatteryVoltage) / BATT_MIN_V;
+        // 向き直し
+        while(abs(imu.yaw) > 5) {
+            bool dir = signbit(imu.yaw);
+            motor.turn(dir);
+        }
 
-        // // 向き直し
-        // while(abs(devices.mpu6500->yaw) > 5) {
-        //     bool dir = signbit(devices.mpu6500->yaw);
-        //     hwc.motor->turn(dir, speed);
-        // }
+        // ラインセンサー処理
+        while(line.isonLine) {
+            motor.run(line.angle);
+        }
 
-        // // ラインセンサー処理
-        // while(hwc.lineSensorAlgo->isOnLine) {
-        //     hwc.motor->run(hwc.lineSensorAlgo->angle, speed);
-        // }
+        // キャッチした場合
+        while(mcu->adcGetValue(MAL::Peripheral_ADC::BallCatchA)) {
+            int16_t GoalAngle = cam.angle[cam.AttackColor];
+            if(abs(GoalAngle) > 15) {
+                // 正面
+                motor.run(GoalAngle);
+            }
+            else if (abs(GoalAngle > 45)) {
+                // 狙える
+                motor.carryBall(GoalAngle, cam.distance[cam.AttackColor], imu.yaw);
+            }
+            else {
+                // 狙えない
+                unsigned long tim = mcu->millis();
+                while(((mcu->millis() - tim) < 2000) && (abs(cam.angle[cam.AttackColor])) > 45) {
+                    motor.run(180);
+                }
+            }
+        }
 
-        // // キャッチした場合
-        // while(devices.mcu->adcGetValue(MAL::Peripheral_ADC::BallCatchA) > Ball_threshold) {
-        //     int16_t GoalAngle = devices.camera->angles[devices.camera->AttackColor];
-        //     if(abs(GoalAngle) > 15) {
-        //         // 正面
-        //         hwc.motor->run(GoalAngle, speed);
-        //     }
-        //     else if (abs(GoalAngle > 45)) {
-        //         // 狙える
-        //         hwc.motor->carryBall(GoalAngle, devices.camera->goalDistance[devices.camera->AttackColor], speed, devices.mpu6500->yaw);
-        //     }
-        //     else {
-        //         // 狙えない
-        //         unsigned long tim = devices.mcu->millis();
-        //         while(((devices.mcu->millis() - tim) < 2000) && (abs(devices.camera->angles[devices.camera->AttackColor])) > 45) {
-        //             hwc.motor->run(180, speed / 2);
-        //         }
-        //     }
-        // }
-
-        // // 回り込み
-        // BallAngle = devices.camera->angles[2];
-
-        // if(abs(BallAngle) < isBallFront) {
-        //     angle = isBallFront;
-        // }
-        // else {
-        //     angle = BallAngle + 30 * ((BallAngle)? -1: 1);
-        // }
-
-        // hwc.motor->run(angle, speed);
+        // 回り込み
+        BallAngle = cam.angle[2];
+        if(abs(BallAngle) < isBallFront) {
+            angle = isBallFront;
+        }
+        else {
+            angle = BallAngle + 30 * ((BallAngle)? -1: 1);
+        }
+        motor.run(angle);
     }
 }
