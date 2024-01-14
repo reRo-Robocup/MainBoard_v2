78c3efeadd6478af3ffa94e6e0c349774375700d
onlydcx <rin.hanaoka@icloud.com>
2024-01-14 12:58:42 +0900

    [update] UART Protocol

--- Core/App/Module/Camera.cpp
+++ Core/App/Module/Camera.cpp
@@ -11,6 +11,8 @@
 #define YELLOW 0
 #define BLUE 1
 
+#define CAM MAL::Peripheral_UART::Cam
+
 camera::camera(MAL* mcu) {
     _mcu = mcu;
 }
@@ -25,26 +27,33 @@ void camera::updateFPS() {
 
 void camera::_read_by_header() {
     /*  UARTプロトコル
-        | header(0xFF) | BallAngle(16bit) | YellowAngle(16bit) | BlueAngle(16bit)|
+        | header[4] | BallAngle(16bit) | YellowAngle(16bit) | BlueAngle(16bit)|
         | BallDis(8bit) | enable(8bit) |
     */
-    uint8_t header = 0xFF;
-    uint8_t _data = _mcu->uartGetChar(MAL::Peripheral_UART::Cam);
-    if(_data == header) {
+    const uint8_t header[4] = {0xFF, 0xFF, 0xFD, 0x00};
+    uint8_t _header_noerr = 0;
+
+    // ヘッダー 受信チェック
+    for(int i = 0; i < 4; i++) {
+        if(_mcu->uartGetChar(CAM) != header[i])
+            _header_noerr++;
+    }
+
+    if(_header_noerr) {
         // 角度 格納
         for(int i = 0; i < 2; i++) {
             uint8_t _Hdata, _Ldata;
-            _Hdata = _mcu->uartGetChar(MAL::Peripheral_UART::Cam);
-            _Ldata = _mcu->uartGetChar(MAL::Peripheral_UART::Cam);
+            _Hdata = _mcu->uartGetChar(CAM);
+            _Ldata = _mcu->uartGetChar(CAM);
             angle[i] = ((_Hdata << 8) & 0x0000FF00) | ((_Ldata << 0) & 0x000000FF);
         }
         // 距離データ 格納
         for(int i = 0; i < 2; i++) {
-            distance[i] = _mcu->uartGetChar(MAL::Peripheral_UART::Cam);
+            distance[i] = _mcu->uartGetChar(CAM);
         }
         // 検出できたか 格納
-        enable[0] = (_mcu->uartGetChar(MAL::Peripheral_UART::Cam) & 0x10000000) << 8;
-        enable[1] = (_mcu->uartGetChar(MAL::Peripheral_UART::Cam) & 0x01000000) << 8;
-        enable[2] = (_mcu->uartGetChar(MAL::Peripheral_UART::Cam) & 0x00100000) << 8;
+        enable[0] = (_mcu->uartGetChar(CAM) & 0x10000000) >> 8;
+        enable[1] = (_mcu->uartGetChar(CAM) & 0x01000000) >> 8;
+        enable[2] = (_mcu->uartGetChar(CAM) & 0x00100000) >> 8;
     }
 }
