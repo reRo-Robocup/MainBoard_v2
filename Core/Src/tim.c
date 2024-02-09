a25f6d8c8dc0ffae94a3afe1213e2100b2127d0e
onlydcx <rin.hanaoka@icloud.com>
2024-02-09 15:39:30 +0900

    [update] tim8を1KHzに設定

--- Core/Src/tim.c
+++ Core/Src/tim.c
@@ -183,9 +183,9 @@ void MX_TIM8_Init(void)
 
   /* USER CODE END TIM8_Init 1 */
   htim8.Instance = TIM8;
-  htim8.Init.Prescaler = 0;
+  htim8.Init.Prescaler = 20;
   htim8.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim8.Init.Period = 65535;
+  htim8.Init.Period = 8570;
   htim8.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim8.Init.RepetitionCounter = 0;
   htim8.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
