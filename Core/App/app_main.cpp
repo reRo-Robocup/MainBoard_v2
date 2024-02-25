bc6ec31fa66430e4dfc8eb2ba99395ae4cbd4c47
onlydcx <rin.hanaoka@icloud.com>
2024-02-25 19:07:07 +0900

    app_mainを整理

--- Core/App/app_main.cpp
+++ Core/App/app_main.cpp
@@ -23,11 +23,9 @@ AttitudeController atc(&mcu, &imu);
 UI ui(&mcu);
 LineSensor line(&mcu, &ui);
 
-// extern "C" {
 void app_init();
 void app_main();
 void app_update();
-// };
 
 void app_init() {
     mcu.init();
@@ -45,10 +43,8 @@ void app_update() {
     imu.update();
     atc.update();
     ui.update();
-    //  printf("app_update\n\r");
 }
 
-// extern "C" {
 void app_main() {
     printf("app_start\n\r");
 
@@ -63,65 +59,9 @@ void app_main() {
     ui.buzzer(1000, 50);
 
     int mode = 0;
+    atc.setMode(2);
 
     while (1) {
-        switch (mode) {
-            case 0:
-                atc.setMode(3);
-                atc.setGoStraightAngle(0);
-                atc.setTurnAngle(10);
-                atc.setGoStraightPower(0.1);
-                // printf("ball_angle: %d\n\r", cam.angle);
-
-                break;
-
-            case 1:
-                break;
-
-            default:
-                break;
-        }
-
-        // if(!isSleep) {
-        //     // 向き直し
-        //     while (abs(imu.yaw) > 5) {
-        //         bool dir = signbit(imu.yaw);
-        //         // motor.turn(dir);
-        //     }
-
-        //     // ラインセンサー処理
-        //     while (line.isonLine) {
-        //         motor.run(line.angle);
-        //     }
-
-        //     // キャッチした場合
-        //     while (mcu.adcGetValue(MAL::Peripheral_ADC::BallCatchA) > BallCatchThreshold[0]) {
-        //         int16_t GoalAngle = cam.angle[cam.AttackColor];
-        //         // 正面
-        //         if (abs(GoalAngle) > 15) {
-        //             motor.run(GoalAngle);
-        //         }
-        //         // 狙える
-        //         else if (abs(GoalAngle > 45)) {
-        //             motor.carryBall(GoalAngle, cam.distance[cam.AttackColor], imu.yaw);
-        //         }
-        //         // 狙えない
-        //         else {
-        //             unsigned long tim = mcu.millis();
-        //             while (((mcu.millis() - tim) < 2000) && (abs(cam.angle[cam.AttackColor])) > 45) {
-        //                 motor.run(180);
-        //             }
-        //         }
-        //     }
-
-        //     // 回り込み
-        //     BallAngle = cam.angle[2];
-        //     if (abs(BallAngle) < isBallFront) {
-        //         angle = isBallFront;
-        //     } else {
-        //         angle = BallAngle + 30 * ((BallAngle) ? -1 : 1);
-        //     }
-        //     motor.run(angle);
-        // }
+        atc.setTurnAngle(180);
     }
 }
