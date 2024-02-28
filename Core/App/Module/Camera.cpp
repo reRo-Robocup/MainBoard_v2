6516c6a93a309f706daaee16226ddc6809e1088b
onlydcx <rin.hanaoka@icloud.com>
2024-02-28 23:49:29 +0900

    [update] ボール角度をカメラ基準に合わせた

--- Core/App/Module/Camera.cpp
+++ Core/App/Module/Camera.cpp
@@ -24,43 +24,6 @@ void camera::init() {
 
 void camera::update() {
     this->_read_via_buffer();
-    // printf("%u\n", _mcu->uartGetChar(MAL::Peripheral_UART::Cam));
-}
-
-void camera::_read_by_header() {
-    /*  - Protocol
-        | header[4] | BallAngle(16bit) | YellowAngle(16bit) | BlueAngle(16bit)|
-        | BallDis(8bit) | enable(8bit) |
-    */
-    const uint8_t header[4] = {0xFF, 0xFF, 0xFD, 0x00};
-    uint8_t errcnt = 0;
-
-    // ヘッダー 受信チェック
-    for (int i = 0; i < 4; i++) {
-        if (_mcu->uartGetChar(CAM) != header[i])
-            errcnt++;
-    }
-
-    // if (errcnt == 0) {
-    //     // 角度 格納
-    //     for (int i = 0; i < 2; i++) {
-    //         uint8_t _Hdata, _Ldata;
-    //         _Hdata = _mcu->uartGetChar(CAM);
-    //         _Ldata = _mcu->uartGetChar(CAM);
-    //         angle[i] = (_Hdata << 8) | _Ldata;
-    //     }
-
-    //     // 距離データ 格納
-    //     for (int i = 0; i < 2; i++) {
-    //         distance[i] = _mcu->uartGetChar(CAM);
-    //     }
-
-    //     // 検出できたか 格納
-    //     uint8_t _data = _mcu->uartGetChar(CAM);
-    //     enable[0] = (bool)_data & 0b00000001;
-    //     enable[1] = (bool)_data & 0b00000010;
-    //     enable[2] = (bool)_data & 0b00000100;
-    // }
 }
 
 void camera::_read_via_buffer() {
@@ -68,7 +31,7 @@ void camera::_read_via_buffer() {
     uint32_t data_size = _mcu->uartGetRxDataSize(CAM);
     _mcu->uartReadViaBuffer(CAM, data, data_size);
 
-    const uint8_t header[4] = {0xFF, 0xFF, 0xFD, 0x00};
+    static const uint8_t header[4] = {0xFF, 0xFF, 0xFD, 0x00};
 
     for (uint32_t i = 0; i < data_size; i++) {
         switch (_rx_mode) {
@@ -110,6 +73,7 @@ void camera::_read_via_buffer() {
                     _rx_mode = 0;
                     _rx_data_index = 0;
                     this->data = camera_rx_data_parser.parsed_data;
+                    this->data.ball_angle = 360 - this->data.ball_angle;
                 }
                 break;
 
