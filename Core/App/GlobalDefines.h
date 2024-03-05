aef1ad92503d4f091cdcbbf083c2cceac5b3d09f
onlydcx <rin.hanaoka@icloud.com>
2024-03-05 21:21:46 +0900

    [update] map関数を追加

--- Core/App/GlobalDefines.h
+++ Core/App/GlobalDefines.h
@@ -14,6 +14,10 @@
 
 #define CONTROLL_CYCLE 0.001  // 1KHz
 
+float map (float x, float in_min, float in_max, float out_min, float out_max) {
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
 #define deg_to_rad (M_PI / 180)
 #define rad_to_deg (180 / M_PI)
 
