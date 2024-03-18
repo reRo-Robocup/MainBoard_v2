77244b588e92a6ca1de2e549d34d91d2cd093fe3
onlydcx <rin.hanaoka@icloud.com>
2024-03-18 23:58:27 +0900

    [add] attacker logic

--- Core/App/Algo/attacker.cpp
+++ Core/App/Algo/attacker.cpp
@@ -22,21 +22,39 @@ void Attacker::init() {
 }
 
 void Attacker::update() {
+    atc->setGoStraightPower(0.9);
     if(line->isonLine) {
         // ライン上
         atc->setMode(3);
         atc->setGoStraightAngle(line->angle);
-        ui->buzzer(1000,1);
+        // ui->buzzer(1000,1);
     }
     else {
-        if(cam->data.isBallDetected) {
-            // ボール見つからない
-            if(cam->data.isInMyArea) {
-                // 待機
-                atc->setMode(0);
+        if(!cam->data.isBallDetected) {
+            atc->setMode(0);
+            ui->buzzer(1000,1);
+        }
+        else {
+            atc->setMode(3);
+            int16_t BallAngle = cam->data.ball_angle;
+            uint8_t distance = cam->data.ball_distance;
+
+            if(abs(BallAngle) <= 10) {
+                atc->setGoStraightAngle(0);
             }
             else {
-                
+                if(distance > 50) {
+                    atc->setGoStraightAngle(BallAngle);
+                }
+                else {
+                    int8_t dir = signbit(BallAngle);
+                    if(dir) {
+                        atc->setGoStraightAngle(BallAngle + 30);
+                    }
+                    else {
+                        atc->setGoStraightAngle(BallAngle - 30);
+                    }
+                }
             }
         }
     }
